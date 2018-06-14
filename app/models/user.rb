45316fdb38088375c6ded1ddebe60884d65eede5
Artur Barashev <artvbar@gmail.com>
2018-06-15 01:47:27 +0300

    Second draft

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,35 @@
-require 'time_difference'
-
 WEEK_IN_SEC = 7 * 24 * 3600
+WeekStats = Struct.new(:total_week_orderers, :first_time_week_orderers)
 
 class User < ApplicationRecord
 
   def get_cohorts(weeks_limit=8)
     cohorts = {}
-    last_time = User.order(:created_at).last.created_at
-    last_key = last_time.to_time.to_i
+    users = User.order(:created_at).reverse
+    latest_time = users.first.created_at.to_time.to_i
 
-    User.order(:created_at).reverse.each do |u|
-      weeks_diff = TimeDifference.between(last_time, u.created_at).in_weeks.to_i
-      break if weeks_diff > weeks_limit
-      key = last_key - (weeks_diff * WEEK_IN_SEC)
+    # Get user ids.
+    users.each do |u|
+      weeks_diff = (latest_time - u.created_at.to_time.to_i)/WEEK_IN_SEC
+      break if weeks_diff >= weeks_limit
+      key = latest_time - (weeks_diff * WEEK_IN_SEC)
 
       if cohorts[key]
-        cohorts[key] << u.id
+        cohorts[key][0] << u.id
       else
-        cohorts[key] = [u.id]
+        cohorts[key] = [[u.id], []]
+      end
+    end
+
+    # Collect order stats.
+    cohorts.keys.sort.reverse.each do |k|
+      k.step(latest_time, WEEK_IN_SEC) do |week_end|
+        week_start = week_end - WEEK_IN_SEC
+
+        orders = Order.where(user_id: cohorts[k][0],
+                             created_at: Time.at(week_start)..Time.at(week_end))
+        cohorts[k][1] << WeekStats.new(orders.distinct.count(:user_id),
+                                       orders.where(order_num: 1).count)
       end
     end
 
